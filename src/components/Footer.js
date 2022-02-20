7bcd93569d56a8837e1bcaf5beaa7af7d429ee33
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-20 18:18:32 -0400

    Add rel="noreferrer"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ const Footer = () => {
     <footer>
       <p className="attribution">
         Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
           Frontend Mentor
         </a>
         . Coded by <a href="https://nakoyawilson.netlify.app/">Nakoya Wilson</a>
